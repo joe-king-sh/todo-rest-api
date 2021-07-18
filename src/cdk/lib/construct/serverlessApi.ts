6a4a6db2f28b4d63274e057e2265a10c1e14df1f
Joe King <a8109058@gmail.com>
2021-07-18 19:43:50 +0900

    Add: getとpostのswaggerの定義を追加

--- src/cdk/lib/construct/serverlessApi.ts
+++ src/cdk/lib/construct/serverlessApi.ts
@@ -1,14 +1,11 @@
 import * as cdk from "@aws-cdk/core";
 import { generateResourceName } from "../utility";
-import cognito = require("@aws-cdk/aws-cognito");
 import * as environment from "../environment";
 import * as lambda from "@aws-cdk/aws-lambda";
 import { ServicePrincipal } from "@aws-cdk/aws-iam";
 import * as logs from "@aws-cdk/aws-logs";
 
 import * as apigw from "@aws-cdk/aws-apigateway";
-import { PermissionsBoundary } from "@aws-cdk/aws-iam";
-import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from "constants";
 
 interface ServerlessApiProps {
   environmentVariables: environment.EnvironmentVariables;
@@ -67,9 +64,15 @@ export class ServerlessApi extends cdk.Construct {
         },
       },
       // TODO このAPI＿IDだけ、デプロイ完了後Swagger.ymlだけ入れ替えてから、仕様書をS３へアップする。もしくはデプロイされた後の仕様書を抜いてきて、入れ替える
-      servers: [{url: `https://$API_ID.execute-api.${props.environmentVariables.region}.amazonaws.com/${env}/`, description:''}],
-
+      servers: [
+        {
+          url: `https://$API_ID.execute-api.${props.environmentVariables.region}.amazonaws.com/${env}/`,
+          description: "",
+        },
+      ],
+      // リソースに関わらず共通の設定をここで定義する
       components: {
+        // Cognitoユーザプールを使用した認証を設定する
         securitySchemes: {
           CognitoAuth: {
             type: "apiKey",
@@ -85,57 +88,189 @@ export class ServerlessApi extends cdk.Construct {
             },
           },
         },
+        // 共通で使用するレスポンスを定義する
+        schemas: {
+          Todo: {
+            type: "object",
+            properties: {
+              todoId: {
+                type: "string",
+              },
+              title: {
+                type: "string",
+              },
+              content: {
+                type: "string",
+              },
+              dueDate: {
+                type: "string",
+                format: "date",
+              },
+              isImportant: {
+                type: "boolean",
+              },
+            },
+          },
+          Error: {
+            type: "object",
+            required: ["message"],
+            properties: {
+              message: {
+                type: "string",
+              },
+            },
+          },
+        },
       },
+      // 各APIのリソースを以下で定義していく
       paths: {
-        "/hello": {
+        "/todos": {
           get: {
-            operationId: "hello",
-            summary: "hello from cdk sample lambda",
+            operationId: "listTodos",
+            tags: ["Todo"],
+            summary: "Todo情報 一括取得API",
+            description: "ユーザーのTodoを指定した件数分返却する",
             security: [
               {
                 CognitoAuth: [],
               },
             ],
             parameters: [
-              // {
-              //   in: 'path',
-              //   name: 'hoge',
-              //   required: true,
-              //   schema: {
-              //     type: 'string'
-              //   }
-              // }
+              {
+                name: "limit",
+                in: "query",
+                description: "取得するTodoの件数を指定する(最大 100件)",
+                required: false,
+                schema: {
+                  type: "integer",
+                  format: "int32",
+                },
+              },
+              {
+                name: "nextToken",
+                in: "query",
+                description:
+                  "次のページを取得する場合に指定する(指定した場合limitは無視される)",
+                required: false,
+                schema: {
+                  type: "string",
+                },
+              },
             ],
-            // requestBody: {
-            //   content: {
-            //     'application/json': {
-            //       schema: {
-            //         properties: {
-            //           foo: {
-            //             type: 'string',
-            //           },
-            //           bar: {
-            //             type: 'integer'
-            //           }
-            //         }
-            //       }
-            //     }
-            //   }
-            // },
             responses: {
               200: {
-                description: "OK",
+                description: "Todo情報のjsonリストを返却する",
                 content: {
                   "application/json": {
                     schema: {
+                      type: "object",
+                      required: ["todos"],
                       properties: {
-                        id: {
-                          type: "integer",
+                        todos: {
+                          type: "array",
+                          items: {
+                            $ref: "#/components/schemas/Todo",
+                          },
                         },
-                        name: {
+                        nextToken: {
                           type: "string",
                         },
                       },
+                    },
+                  },
+                },
+              },
+              default: {
+                description: "予期せぬエラーが発生",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "#/components/schemas/Error",
+                    },
+                  },
+                },
+              },
+            },
+            "x-amazon-apigateway-integration": {
+              uri:
+                "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/arn:${AWS::Partition}:lambda:${AWS::Region}:${AWS::AccountId}:function:" +
+                helloLambda.functionName +
+                "/invocations",
+              responses: {
+                default: {
+                  statusCode: "200",
+                },
+              },
+              passthroughBehavior: "when_no_match",
+              httpMethod: "POST",
+              contentHandling: "CONVERT_TO_TEXT",
+              type: "aws_proxy",
+            },
+          },
+          post: {
+            operationId: "createTodos",
+            tags: ["Todo"],
+            summary: "Todo情報 登録API",
+            description: "ユーザーのTodo情報を1件登録する",
+            security: [
+              {
+                CognitoAuth: [],
+              },
+            ],
+            requestBody: {
+              description: "Todo情報をシステムに登録する",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    required: ["title", "content"],
+                    properties: {
+                      title: {
+                        type: "string",
+                      },
+                      content: {
+                        type: "string",
+                      },
+                      dueDate: {
+                        type: "string",
+                        format: "date",
+                      },
+                      isImportant: {
+                        type: "boolean",
+                      },
+                    },
+                  },
+                  examples: {
+                    Todo: {
+                      summary: "Todo情報 登録例",
+                      value: {
+                        title: "今日やること",
+                        content: "ゴミを捨てる",
+                        dueDate: "2021-07-18",
+                        isImportant: false,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            responses: {
+              200: {
+                description: "Todo情報の登録が正常に完了",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "#/components/schemas/Todo",
+                    },
+                  },
+                },
+              },
+              default: {
+                description: "予期せぬエラーが発生",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "#/components/schemas/Error",
                     },
                   },
                 },
@@ -204,28 +339,27 @@ export class ServerlessApi extends cdk.Construct {
 
     // TODO  API ドキュメント公開用 S3Bucketの作成
 
-    // const fs = require("fs");
-    // const yaml = require("js-yaml");
+    const fs = require("fs");
+    const yaml = require("js-yaml");
 
-    // const yamlText = yaml.dump(this.swagger);
-    // fs.writeFile("./cdk.out/swagger.yaml", yamlText, "utf8", (err: any) => {
-    //   if (err) {
-    //     console.error(err.message);
-    //     process.exit(1);
-    //   }
-    //   console.log("SwaggerファイルをYamlで出力しました");
-    // });
+    const yamlText = yaml.dump(this.swagger);
+    fs.writeFile("./cdk.out/swagger.yaml", yamlText, "utf8", (err: any) => {
+      if (err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      console.log("SwaggerファイルをYamlで出力しました");
+    });
     // TODO ドキュメントを各環境毎のS3へアップロードするやつ。Synthで動いてしまうから、S３Put自体は、テスト通った後の方がいいかも　ここではcdk.outに吐くまで
 
     // APIキーとかも作られるまでは、わからないので、それを事前にSwaggerに入れることはできない。
     // Deploy完了後のoutputから取得してスクリプトで埋め込むことにする。
-        // // swaggerにサーバー情報を追加
-        // this.swagger.servers = [
-        //   {
-        //     url: api.arnForExecuteApi,
-        //     description: `${env} 環境`,
-        //   },
-        // ];
-    
+    // // swaggerにサーバー情報を追加
+    // this.swagger.servers = [
+    //   {
+    //     url: api.arnForExecuteApi,
+    //     description: `${env} 環境`,
+    //   },
+    // ];
   }
 }
